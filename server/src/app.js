c1df20959b0dee1a52fbd476acd5e51b29c4de8a
Petkoneo <petkoneo@gmail.com>
2018-01-28 18:04:12 +0100

    Sequelize start

--- server/src/app.js
+++ server/src/app.js
@@ -2,18 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
-
+const {sequelize} = require("./models");
+const config = require("./config/config");
 
 const app = express();
 app.use(morgan("combined"));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/register", (req,res) => {
-    res.send({
-    message: "Hello ${req.body.email}! You user was registered! Have fun."
-});
+require("./routes")(app);
+
+sequelize.sync().then(() => {
+    app.listen(config.port);
+    console.log(`Server started on port ${config.port}`);
 })
-
-
-app.listen(process.env.PORT || 8081);
