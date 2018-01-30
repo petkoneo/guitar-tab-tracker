20a13c891dbd705f97d4835303373b2b59114e70
Petkoneo <petkoneo@gmail.com>
2018-01-30 23:21:13 +0100

    Login setting... with some outcome

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,4 +1,13 @@
 const {User} = require("../models");
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+function jwtSignUser (user){
+    const ONE_WEEK =  60*60*60*24*7
+    return jwt.sign(user, config.authentication.jwtSecret,{
+        expiresIn: ONE_WEEK
+    })
+}
 
 module.exports = {
     async register(req, res){
@@ -22,7 +31,7 @@ module.exports = {
                 });
                 if(!user){
                     return res.status(403).send({
-                        error: "Login nie je platny"
+                        error: "Login nie je platný."
                     })
                 }
 
@@ -35,7 +44,8 @@ module.exports = {
 
                     const userJson = user.toJSON();
                     res.send ({
-                        user: userJson
+                        user: userJson,
+                        token: jwtSignUser(userJson)
                     })
                 } catch(err){
                 res.status(500).send({
